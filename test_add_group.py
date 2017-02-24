17a648decdb0f0e7e5c3e9ad830930dca1eb8646
wlansk <registr50@gmail.com>
2017-02-24 16:54:31 +0200

    Корректировка №2 файла test_add_group.py

--- test_add_group.py
+++ test_add_group.py
@@ -18,7 +18,7 @@ class test_add_group(unittest.TestCase):
     def test_test_add_group(self):
         success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get('http://localhost/addressbook/')
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")

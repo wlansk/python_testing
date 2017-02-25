15498919711dd987c5272fe143fcc060662f04c9
wlansk <registr50@gmail.com>
2017-02-25 14:35:05 +0200

    Рефакторинг кода в файле test_add_group.py: создание методов в классе, удаление лишних комментариев.

--- test_add_group.py
+++ test_add_group.py
@@ -13,12 +13,11 @@ class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
-    def test_test_add_group(self):
-        wd = self.wd
-        # open home page
+
+    def open_home_page(self, wd):
         wd.get('http://localhost/addressbook/')
-        # login
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -26,11 +25,14 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-        # open groups page
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd):
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group firm
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("proba1")
@@ -42,10 +44,21 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("Forever")
         # submit group creation
         wd.find_element_by_name("submit").click()
-        # return to groups page
+
+    def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
-        # logout
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
+
+    def test_test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()

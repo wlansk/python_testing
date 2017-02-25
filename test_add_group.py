83b5acd952bada8ec8299e22533510e9eb7616e1
wlansk <registr50@gmail.com>
2017-02-25 13:38:00 +0200

    Корректировка №3 файла test_add_group.py

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
 
 def is_alert_present(wd):
     try:

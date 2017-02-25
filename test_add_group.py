435db73a8d10a46356c29975eab0256aa92c9b31
wlansk <registr50@gmail.com>
2017-02-25 15:15:28 +0200

    Улучшение тестового сценария в файле test_add_group.py: создание нового метода на основе существующего методом copy->past.

--- test_add_group.py
+++ test_add_group.py
@@ -51,12 +51,21 @@ class test_add_group(unittest.TestCase):
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def test_test_add_group(self):
+    def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
         self.create_group(wd, name="proba1", header="Prob", footer="Forever")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
         self.return_to_groups_page(wd)
         self.logout(wd)
 

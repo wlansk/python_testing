bcaa3625568e185aa9eb8673979ca57e03b0e092
wlansk <registr50@gmail.com>
2017-02-28 14:30:51 +0200

    Создание нового теста test_del_group.py

--- fixture/group.py
+++ fixture/group.py
@@ -11,7 +11,7 @@ class GroupHelper:
         wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
-        wd = self.wd
+        wd = self.app.wd
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
@@ -27,6 +27,15 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):

1d974a8f5e5f7abe50adba7976af511f40ae8b86
wlansk <registr50@gmail.com>
2017-02-28 11:12:41 +0200

    Тесты приводим к API: убираем лишние методы.

--- test_add_group.py
+++ test_add_group.py
@@ -54,20 +54,14 @@ class test_add_group(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="proba1", header="Prob", footer="Forever"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def tearDown(self):

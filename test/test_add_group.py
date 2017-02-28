fec7aba1913af6fe74bbfef7e1a8141a33ade950
wlansk <registr50@gmail.com>
2017-02-28 13:09:53 +0200

    Созданы пакеты для разных зон ответственности.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from group import Group
-from application import Application
+
+from model.group import Group
+from fixture.application import Application
+
 
 @pytest.fixture
 def app(request):

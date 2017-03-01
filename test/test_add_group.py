c0cfcfd621b0cf1a6b8f8379eecb5e156a75de7a
wlansk <registr50@gmail.com>
2017-03-01 13:20:31 +0200

    Создание интеллектуального логина.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,6 +6,7 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="proba1", header="Prob", footer="Forever"))
 
+
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
 

a1dfbf0cdeacc6efc8e5d26e1cc200748ba95789
wlansk <registr50@gmail.com>
2017-03-03 13:37:31 +0200

    Генератор случайных данных с комбинациями пустых и случайных строк в цикле с раскомментированным сценарием.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -19,13 +19,12 @@ testdata = [
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
-    pass
-    #old_groups = app.group.get_group_list()
-    #app.group.create(group)
-    #assert len(old_groups) + 1 == app.group.count()
-    #new_groups = app.group.get_group_list()
-    #old_groups.append(group)
-    #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    old_groups = app.group.get_group_list()
+    app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
+    new_groups = app.group.get_group_list()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 

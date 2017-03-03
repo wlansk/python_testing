25f576955575d8550ddbf040cfd52ebb86785821
wlansk <registr50@gmail.com>
2017-03-03 12:57:26 +0200

    Тестовые данные внутри цикла.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,22 +4,19 @@ from model.group import Group
 
 
 def test_add_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="proba1", header="Prob", footer="Forever")
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    testdata [
+        Group(name="proba1", header="Prob", footer="Forever"),
+        Group(name="", header="", footer="")
+    ]
+    for group in testdata:
+        old_groups = app.group.get_group_list()
+        app.group.create(group)
+        assert len(old_groups) + 1 == app.group.count()
+        new_groups = app.group.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 

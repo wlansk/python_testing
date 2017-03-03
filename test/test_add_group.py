d6dfdf857525d605ae6c4770c14433faa44c6559
wlansk <registr50@gmail.com>
2017-03-03 13:48:20 +0200

    Генератор случайных данных - добавление случайностей.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,15 +6,13 @@ import random
 import string
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [
-        Group(name=name, header=header, footer=footer)
-        for name in ["", random_string("name", 10)]
-        for heager in ["", random_string("header", 20)]
-        for footer in ["", random_string("footer", 20)]
+testdata = [Group(name="", header="", footer="")] + [
+        Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+        for i in range(5)
     ]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])

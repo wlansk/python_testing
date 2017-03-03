29215293effd806ccbc979de764982f2b78aaa9a
wlansk <registr50@gmail.com>
2017-03-03 13:04:28 +0200

    Вынос тестовых данных из функции.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from model.group import Group
+import pytest
 
-
-def test_add_group(app):
-    testdata [
+testdata = [
         Group(name="proba1", header="Prob", footer="Forever"),
         Group(name="", header="", footer="")
     ]
-    for group in testdata:
-        old_groups = app.group.get_group_list()
-        app.group.create(group)
-        assert len(old_groups) + 1 == app.group.count()
-        new_groups = app.group.get_group_list()
-        old_groups.append(group)
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+@pytest.mark.parametrize("group", testdata)
+def test_add_group(app, group):
+    pass
+    #old_groups = app.group.get_group_list()
+    #app.group.create(group)
+    #assert len(old_groups) + 1 == app.group.count()
+    #new_groups = app.group.get_group_list()
+    #old_groups.append(group)
+    #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 

a9471134077dd3d336bddf7317c99e4d1b418da0
wlansk <registr50@gmail.com>
2017-03-06 14:06:19 +0200

    Генерация тестовых данных в файл.

--- data/add_group.py
+++ data/add_group.py
@@ -7,12 +7,4 @@ constant = [
     Group(name="name1", header="header1", footer="footer1"),
 ]
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-
-testdata = [Group(name="", header="", footer="")] + [
-        Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-        for i in range(5)
-]

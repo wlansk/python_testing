dd1666eebcb47be5a5dbc47bc4318ebe35a87dc5
wlansk <registr50@gmail.com>
2017-03-07 14:05:41 +0200

    Создание нового теста test_db_matches_ui.py.

--- fixture/db.py
+++ fixture/db.py
@@ -19,7 +19,7 @@ class DbFixture:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
                 (id, name, header, footer) = row
-                list.append(Group(id=id, name=name, header=header, footer=footer))
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
         finally:
             cursor.close()
         return list
